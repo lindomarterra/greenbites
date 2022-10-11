4335670ae0546fcafa4cc1ecfe2796930b6b223b
Lindomar Terra <102319307+lindomarterra@users.noreply.github.com>
2022-10-11 11:37:00 -0300

    Add files via upload

---
+++ script(menu).js
@@ -0,0 +1,468 @@
+//  OPTION 1
+
+let majorSignal1 = document.getElementById('majorSignal1')
+let minorSignal1 = document.getElementById('minorSignal1')
+let discription1 = document.getElementById('discription1')
+
+majorSignal1.addEventListener('click', function(){
+
+    majorSignal1.style.display = 'none'
+    minorSignal1.style.display = 'block'
+    discription1.style.display = 'block'
+})
+
+minorSignal1.addEventListener('click', function(){
+
+    majorSignal1.style.display = 'block'
+    minorSignal1.style.display = 'none'
+    discription1.style.display = 'none'
+
+})
+
+//  OPTION 2
+
+let majorSignal2 = document.getElementById('majorSignal2')
+let minorSignal2 = document.getElementById('minorSignal2')
+let discription2 = document.getElementById('discription2')
+
+majorSignal2.addEventListener('click', function(){
+
+    majorSignal2.style.display = 'none'
+    minorSignal2.style.display = 'block'
+    discription2.style.display = 'block'
+})
+
+minorSignal2.addEventListener('click', function(){
+
+    majorSignal2.style.display = 'block'
+    minorSignal2.style.display = 'none'
+    discription2.style.display = 'none'
+
+})
+
+//  OPTION 3
+
+let majorSignal3 = document.getElementById('majorSignal3')
+let minorSignal3 = document.getElementById('minorSignal3')
+let discription3 = document.getElementById('discription3')
+
+majorSignal3.addEventListener('click', function(){
+
+    majorSignal3.style.display = 'none'
+    minorSignal3.style.display = 'block'
+    discription3.style.display = 'block'
+})
+
+minorSignal3.addEventListener('click', function(){
+
+    majorSignal3.style.display = 'block'
+    minorSignal3.style.display = 'none'
+    discription3.style.display = 'none'
+
+})
+
+//  OPTION 4
+
+let majorSignal4 = document.getElementById('majorSignal4')
+let minorSignal4 = document.getElementById('minorSignal4')
+let discription4 = document.getElementById('discription4')
+
+majorSignal4.addEventListener('click', function(){
+
+    majorSignal4.style.display = 'none'
+    minorSignal4.style.display = 'block'
+    discription4.style.display = 'block'
+})
+
+minorSignal4.addEventListener('click', function(){
+
+    majorSignal4.style.display = 'block'
+    minorSignal4.style.display = 'none'
+    discription4.style.display = 'none'
+
+})
+
+//  OPTION 5
+
+let majorSignal5 = document.getElementById('majorSignal5')
+let minorSignal5 = document.getElementById('minorSignal5')
+let discription5 = document.getElementById('discription5')
+
+majorSignal5.addEventListener('click', function(){
+
+    majorSignal5.style.display = 'none'
+    minorSignal5.style.display = 'block'
+    discription5.style.display = 'block'
+})
+
+minorSignal5.addEventListener('click', function(){
+
+    majorSignal5.style.display = 'block'
+    minorSignal5.style.display = 'none'
+    discription5.style.display = 'none'
+
+})
+
+//  OPTION 6
+
+let majorSignal6 = document.getElementById('majorSignal6')
+let minorSignal6 = document.getElementById('minorSignal6')
+let discription6 = document.getElementById('discription6')
+
+majorSignal6.addEventListener('click', function(){
+
+    majorSignal6.style.display = 'none'
+    minorSignal6.style.display = 'block'
+    discription6.style.display = 'block'
+})
+
+minorSignal6.addEventListener('click', function(){
+
+    majorSignal6.style.display = 'block'
+    minorSignal6.style.display = 'none'
+    discription6.style.display = 'none'
+
+})
+
+//  OPTION 7
+
+let majorSignal7 = document.getElementById('majorSignal7')
+let minorSignal7 = document.getElementById('minorSignal7')
+let discription7 = document.getElementById('discription7')
+
+majorSignal7.addEventListener('click', function(){
+
+    majorSignal7.style.display = 'none'
+    minorSignal7.style.display = 'block'
+    discription7.style.display = 'block'
+})
+
+minorSignal7.addEventListener('click', function(){
+
+    majorSignal7.style.display = 'block'
+    minorSignal7.style.display = 'none'
+    discription7.style.display = 'none'
+
+})
+
+//  OPTION 8
+
+let majorSignal8 = document.getElementById('majorSignal8')
+let minorSignal8 = document.getElementById('minorSignal8')
+let discription8 = document.getElementById('discription8')
+
+majorSignal8.addEventListener('click', function(){
+
+    majorSignal8.style.display = 'none'
+    minorSignal8.style.display = 'block'
+    discription8.style.display = 'block'
+})
+
+minorSignal8.addEventListener('click', function(){
+
+    majorSignal8.style.display = 'block'
+    minorSignal8.style.display = 'none'
+    discription8.style.display = 'none'
+
+})
+
+//  OPTION 9
+
+let majorSignal9 = document.getElementById('majorSignal9')
+let minorSignal9 = document.getElementById('minorSignal9')
+let discription9 = document.getElementById('discription9')
+
+majorSignal9.addEventListener('click', function(){
+
+    majorSignal9.style.display = 'none'
+    minorSignal9.style.display = 'block'
+    discription9.style.display = 'block'
+})
+
+minorSignal9.addEventListener('click', function(){
+
+    majorSignal9.style.display = 'block'
+    minorSignal9.style.display = 'none'
+    discription9.style.display = 'none'
+
+})
+
+//  OPTION 10
+
+let majorSignal10 = document.getElementById('majorSignal10')
+let minorSignal10 = document.getElementById('minorSignal10')
+let discription10 = document.getElementById('discription10')
+
+majorSignal10.addEventListener('click', function(){
+
+    majorSignal10.style.display = 'none'
+    minorSignal10.style.display = 'block'
+    discription10.style.display = 'block'
+})
+
+minorSignal10.addEventListener('click', function(){
+
+    majorSignal10.style.display = 'block'
+    minorSignal10.style.display = 'none'
+    discription10.style.display = 'none'
+
+})
+
+//  OPTION 11
+
+let majorSignal11 = document.getElementById('majorSignal11')
+let minorSignal11 = document.getElementById('minorSignal11')
+let discription11 = document.getElementById('discription11')
+
+majorSignal11.addEventListener('click', function(){
+
+    majorSignal11.style.display = 'none'
+    minorSignal11.style.display = 'block'
+    discription11.style.display = 'block'
+})
+
+minorSignal11.addEventListener('click', function(){
+
+    majorSignal11.style.display = 'block'
+    minorSignal11.style.display = 'none'
+    discription11.style.display = 'none'
+
+})
+
+//  OPTION 12
+
+let majorSignal12 = document.getElementById('majorSignal12')
+let minorSignal12 = document.getElementById('minorSignal12')
+let discription12 = document.getElementById('discription12')
+
+majorSignal12.addEventListener('click', function(){
+
+    majorSignal12.style.display = 'none'
+    minorSignal12.style.display = 'block'
+    discription12.style.display = 'block'
+})
+
+minorSignal12.addEventListener('click', function(){
+
+    majorSignal12.style.display = 'block'
+    minorSignal12.style.display = 'none'
+    discription12.style.display = 'none'
+
+})
+
+//  OPTION 13
+
+let majorSignal13 = document.getElementById('majorSignal13')
+let minorSignal13 = document.getElementById('minorSignal13')
+let discription13 = document.getElementById('discription13')
+
+majorSignal13.addEventListener('click', function(){
+
+    majorSignal13.style.display = 'none'
+    minorSignal13.style.display = 'block'
+    discription13.style.display = 'block'
+})
+
+minorSignal13.addEventListener('click', function(){
+
+    majorSignal13.style.display = 'block'
+    minorSignal13.style.display = 'none'
+    discription13.style.display = 'none'
+
+})
+
+//  OPTION 14
+
+
+let majorSignal14 = document.getElementById('majorSignal14')
+let minorSignal14 = document.getElementById('minorSignal14')
+let discription14 = document.getElementById('discription14')
+
+majorSignal14.addEventListener('click', function(){
+
+    majorSignal14.style.display = 'none'
+    minorSignal14.style.display = 'block'
+    discription14.style.display = 'block'
+})
+
+minorSignal14.addEventListener('click', function(){
+
+    majorSignal14.style.display = 'block'
+    minorSignal14.style.display = 'none'
+    discription14.style.display = 'none'
+
+})
+
+//  OPTION 15
+
+let majorSignal15 = document.getElementById('majorSignal15')
+let minorSignal15 = document.getElementById('minorSignal15')
+let discription15 = document.getElementById('discription15')
+
+majorSignal15.addEventListener('click', function(){
+
+    majorSignal15.style.display = 'none'
+    minorSignal15.style.display = 'block'
+    discription15.style.display = 'block'
+})
+
+minorSignal15.addEventListener('click', function(){
+
+    majorSignal15.style.display = 'block'
+    minorSignal15.style.display = 'none'
+    discription15.style.display = 'none'
+
+})
+
+//  OPTION 16
+
+
+let majorSignal16 = document.getElementById('majorSignal16')
+let minorSignal16 = document.getElementById('minorSignal16')
+let discription16 = document.getElementById('discription16')
+
+majorSignal16.addEventListener('click', function(){
+
+    majorSignal16.style.display = 'none'
+    minorSignal16.style.display = 'block'
+    discription16.style.display = 'block'
+})
+
+minorSignal16.addEventListener('click', function(){
+
+    majorSignal16.style.display = 'block'
+    minorSignal16.style.display = 'none'
+    discription16.style.display = 'none'
+
+})
+
+//  OPTION 17
+
+let majorSignal17 = document.getElementById('majorSignal17')
+let minorSignal17 = document.getElementById('minorSignal17')
+let discription17 = document.getElementById('discription17')
+
+majorSignal17.addEventListener('click', function(){
+
+    majorSignal17.style.display = 'none'
+    minorSignal17.style.display = 'block'
+    discription17.style.display = 'block'
+})
+
+minorSignal17.addEventListener('click', function(){
+
+    majorSignal17.style.display = 'block'
+    minorSignal17.style.display = 'none'
+    discription17.style.display = 'none'
+
+})
+
+//  OPTION 18
+
+let majorSignal18 = document.getElementById('majorSignal18')
+let minorSignal18 = document.getElementById('minorSignal18')
+let discription18 = document.getElementById('discription18')
+
+majorSignal18.addEventListener('click', function(){
+
+    majorSignal18.style.display = 'none'
+    minorSignal18.style.display = 'block'
+    discription18.style.display = 'block'
+})
+
+minorSignal18.addEventListener('click', function(){
+
+    majorSignal18.style.display = 'block'
+    minorSignal18.style.display = 'none'
+    discription18.style.display = 'none'
+
+})
+
+
+//  OPTION 19
+
+let majorSignal19 = document.getElementById('majorSignal19')
+let minorSignal19 = document.getElementById('minorSignal19')
+let discription19 = document.getElementById('discription19')
+
+majorSignal19.addEventListener('click', function(){
+
+    majorSignal19.style.display = 'none'
+    minorSignal19.style.display = 'block'
+    discription19.style.display = 'block'
+})
+
+minorSignal19.addEventListener('click', function(){
+
+    majorSignal19.style.display = 'block'
+    minorSignal19.style.display = 'none'
+    discription19.style.display = 'none'
+
+})
+
+
+//  OPTION 20
+
+let majorSignal20 = document.getElementById('majorSignal20')
+let minorSignal20 = document.getElementById('minorSignal20')
+let discription20 = document.getElementById('discription20')
+
+majorSignal20.addEventListener('click', function(){
+
+    majorSignal20.style.display = 'none'
+    minorSignal20.style.display = 'block'
+    discription20.style.display = 'block'
+})
+
+minorSignal20.addEventListener('click', function(){
+
+    majorSignal20.style.display = 'block'
+    minorSignal20.style.display = 'none'
+    discription20.style.display = 'none'
+
+})
+
+//  OPTION 21
+
+let majorSignal21 = document.getElementById('majorSignal21')
+let minorSignal21 = document.getElementById('minorSignal21')
+let discription21 = document.getElementById('discription21')
+
+majorSignal21.addEventListener('click', function(){
+
+    majorSignal21.style.display = 'none'
+    minorSignal21.style.display = 'block'
+    discription21.style.display = 'block'
+})
+
+minorSignal21.addEventListener('click', function(){
+
+    majorSignal21.style.display = 'block'
+    minorSignal21.style.display = 'none'
+    discription21.style.display = 'none'
+
+})
+
+
+//  OPTION 22
+
+let majorSignal22 = document.getElementById('majorSignal22')
+let minorSignal22 = document.getElementById('minorSignal22')
+let discription22 = document.getElementById('discription22')
+
+majorSignal22.addEventListener('click', function(){
+
+    majorSignal22.style.display = 'none'
+    minorSignal22.style.display = 'block'
+    discription22.style.display = 'block'
+})
+
+minorSignal22.addEventListener('click', function(){
+
+    majorSignal22.style.display = 'block'
+    minorSignal22.style.display = 'none'
+    discription22.style.display = 'none'
+
+})
+
+
