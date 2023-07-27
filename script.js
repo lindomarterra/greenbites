960d482973e39fc688b2b077d40c313477a09418
lindomarterra <terra.lindomar@gmail.com>
2023-07-26 21:13:08 -0300

    july-26

---
+++ script.js
@@ -0,0 +1,7 @@
+
+/*function mouseOver(){
+    document.getElementById('navBar').classList.add('eventScrool')
+}
+function mouseOut(){
+    document.getElementById('navBar').classList.remove('eventScrool')
+}*/
