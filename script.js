68664518438afa1b26fecd1ce1b1269de8156296
lindomarterra <terra.lindomar@gmail.com>
2023-07-30 17:41:09 -0300

    july 30

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-let banner01=document.getElementById('banner01')
-let banner02=document.getElementById('banner02')
-let banner03=document.getElementById('banner03')
-let banner04=document.getElementById('banner04')
-let interval= 4000
+window.addEventListener('load', ()=>{
 
-function slide1(){
-    banner01.classList.add('bannerHide')
-    banner02.classList.remove('bannerHide')
-    setTimeout('slide2()', interval)
-}
-function slide2(){
-    banner02.classList.add('bannerHide')
-    banner03.classList.remove('bannerHide')
-    setTimeout('slide3()', interval)
-}
-function slide3(){
-    banner03.classList.add('bannerHide')
-    banner04.classList.remove('bannerHide')
-    setTimeout('slide4()', interval)
-}
-function slide4(){
-    banner04.classList.add('bannerHide')
-    banner01.classList.remove('bannerHide')
-    setTimeout('slide1()', interval)
-}
+    document.querySelector('.titleBites').classList.add('animationSlideDownOnBites')
+    document.querySelector('.titleGreen').classList.add('animationSlideDownOnGreen')
 
+
+})
 
 
 
