9afb1e255266472c4e298d7af58439f2894d7fcf
lindomarterra <terra.lindomar@gmail.com>
2023-08-01 10:53:53 -0300

    august 1

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+
+//ANIMATION ON TITLE HEADER
 window.addEventListener('load', ()=>{
 
     document.querySelector('.titleBites').classList.add('animationSlideDownOnBites')
     document.querySelector('.titleGreen').classList.add('animationSlideDownOnGreen')
-
-
 })
 
 
 
-/*function mouseOver(){
+//SCROOL EVENT
+function mouseOver(){
     document.getElementById('navBar').classList.add('eventScrool')
 }
 function mouseOut(){
     document.getElementById('navBar').classList.remove('eventScrool')
-}*/
+}
