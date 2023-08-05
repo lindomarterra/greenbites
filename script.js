e31aa9b3a1f4aa4458631614bf26bf6262beedc4
lindomarterra <terra.lindomar@gmail.com>
2023-08-05 11:32:47 -0300

    ago 5

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 //HOME
 //ANIMATION ON TITLE HEADER
-/*window.addEventListener('load', ()=>{
+window.addEventListener('load', ()=>{
 
     document.querySelector('.titleBites').classList.add('animationSlideDownOnBites')
     document.querySelector('.titleGreen').classList.add('animationSlideDownOnGreen')
 })
-*/
 
 
 
@@ -14,32 +13,8 @@
 
 
 
-//MENU
-//ADDING PRODUCT
-const btn=document.getElementsByClassName('btn')
-for(let i=0; i< btn.length; i++){
-    btn[i].addEventListener('click', addingProductToCart)    
-}
 
-function addingProductToCart(event){
 
-    const button = event.target
-    const productInfos = button.parentElement.parentElement
-    const productImage = productInfos.getElementsByClassName("product-image")[0].src
-    const productTitle = productInfos.getElementsByClassName("product-title")[0].innerText
-    const productPrice = productInfos.getElementsByClassName("product-price")[0].innerText
-    
-    const myObj = {
-            name: productTitle, 
-            price: productPrice, 
-            image: productImage,
-            qtd:1
-        }
-    const myJSON = JSON.stringify(myObj)
-    localStorage.setItem("prodAddedCart", myJSON)
-    window.location='index(cart).html'    
-   
-}
 
 
 
